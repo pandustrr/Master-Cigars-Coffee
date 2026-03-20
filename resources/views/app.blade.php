ebdbc9c40eda972e29629c8b018ccfb0bd0ac29e
Pandu Satria <138671942+pandustrr@users.noreply.github.com>
2026-03-20 13:23:04 +0700

    icon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,8 +5,11 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <link rel="icon" type="image/png" href="/icon.png">
+        <link rel="apple-touch-icon" href="/icon.png">
 
         <!-- Fonts -->
+
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
